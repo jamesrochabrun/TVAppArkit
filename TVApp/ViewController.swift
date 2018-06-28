747acf1f6b4e0db3926cad29dfc8a678baf72f6f
James Rochabrun <j@Jamess-MacBook-Pro-3.local>
2018-06-27 17:22:58 -0700

    first commit

--- TVApp/ViewController.swift
+++ TVApp/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 import SceneKit
 import ARKit
+import SpriteKit
+import AVFoundation
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
@@ -20,14 +22,57 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        sceneView.isPlaying = true
+        
+        // 1 creat a spritekit video node and specify which video we want to play
+        guard let spriteKitScene = createSpriteKitScene() else { return }
+        // 2 Create a Scenkit plane and add the spritekit scene as its material
+        let planes = createSceneKitPlane(in: spriteKitScene)
+        // 3 add the tv and the black box to the scene
+        planes.map { scene.rootNode.addChildNode($0) }
+    }
+    
+    private func createSpriteKitScene() -> SKScene? {
+        
+        // 1.1 A SpriteKit scene to contain the Spritekit video node
+        let spriteKitScene = SKScene(size: CGSize(width: sceneView.frame.width, height: sceneView.frame.height))
+        spriteKitScene.scaleMode =  .aspectFit
+        
+        // 1.2 Create a video player, which will be responsible for the playback pf the video material
+        guard let videoUrl = Bundle.main.url(forResource: "peru", withExtension: "mp4") else { return nil }
+        let videoPlayer = AVPlayer(url: videoUrl)
+        
+        // 1.3 Create the Sprite kit video node, containing the video player
+        let videoSpriteKitNode = SKVideoNode(avPlayer: videoPlayer)
+        videoSpriteKitNode.position = CGPoint(x: spriteKitScene.size.width / 2.0, y: spriteKitScene.size.height / 2.0)
+        videoSpriteKitNode.size = spriteKitScene.size
+        videoSpriteKitNode.yScale = -1.0 // this will help to not be upside down
+        videoSpriteKitNode.play()
+        spriteKitScene.addChild(videoSpriteKitNode)
+        
+        return spriteKitScene
+    }
+    
+    func createSceneKitPlane(in scene: SKScene) -> [SCNNode] {
+        
+        let backGround = SCNPlane(width: CGFloat(0.288*2), height: CGFloat(0.18*2))
+        backGround.firstMaterial?.diffuse.contents = scene
+        let backGroundNode = SCNNode(geometry: backGround)
+        backGroundNode.position = SCNVector3(0,0,-1) // 3D one meter in front of us
+        
+        /// do the box for the tv
+        let backGroundBox = SCNBox.init(width: backGround.width*1.1, height: backGround.height*1.1, length: 0.025, chamferRadius: 0)
+        backGroundBox.firstMaterial?.diffuse.contents = UIColor.black
+        let backGroundBoxNode = SCNNode(geometry: backGroundBox)
+        backGroundBoxNode.position = SCNVector3(0,0,-1.013) // 3D one meter in front of us
+        
+        return [backGroundNode, backGroundBoxNode]
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,34 +92,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
